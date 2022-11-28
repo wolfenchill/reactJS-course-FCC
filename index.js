d696436f263c7464ba8c9beb69187c0d28052933
wolfenchill <44275828+wolfenchill@users.noreply.github.com>
2022-11-28 13:37:15 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 // import React from "react";
 // import ReactDOM from "react-DOM";
 
-ReactDOM.render( < h1 > Hi beb < /h1>, document.getElementById("root"));
+// function MainContent() {
+//   return <h1> Hi I 'm learning React</h1>;
+// }
+
+// ReactDOM.render(
+//   <div>
+//     <MainContent />
+//   </div>,
+//   document.getElementById("root")
+// );
+
+const h1Element = document.createElement("h1");
+h1Element.innerText = "hi there";
+h1Element.className = "header";
+
+const rootEle = document.getElementById("root");
+
+rootEle.appendChild(h1Element);
+
+const page = (
+  <div>
+    <h1>i</h1>
+  </div>
+);
